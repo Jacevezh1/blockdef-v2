4800f4e7177317d4c5005eb2c5ed9f6e94762231
jacevezh <jaceves@tokenstate.app>
2025-07-18 07:03:47 -0600

    added next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
